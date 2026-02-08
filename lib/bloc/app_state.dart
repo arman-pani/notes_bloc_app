d0c749a17f07ba8ed483a37d26e1b6b666e40520
Arman Pani <itsarmanpani@gmail.com>
2026-02-08 18:29:41 +0530

    made some changes

--- lib/bloc/app_state.dart
+++ lib/bloc/app_state.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
 import 'package:notes_bloc_app/models.dart';
 
@@ -28,4 +29,31 @@ class AppState {
         'loginHandle': loginHandle,
         'fetchedNotes': fetchedNotes,
       }.toString();
+
+  @override
+  bool operator ==(covariant AppState other) {
+    final otherPropertiesAreEqual = isLoading == other.isLoading &&
+        loginError == other.loginError &&
+        loginHandle == other.loginHandle;
+
+    if (fetchedNotes == null && other.fetchedNotes == null) {
+      return otherPropertiesAreEqual;
+    } else {
+      return otherPropertiesAreEqual &&
+          (fetchedNotes?.isEqualTo(other.fetchedNotes) ?? false);
+    }
+  }
+
+  @override
+  int get hashCode => Object.hash(
+        isLoading,
+        loginError,
+        loginHandle,
+        fetchedNotes,
+      );
+}
+
+extension UnorderedEquality on Object {
+  bool isEqualTo(other) =>
+      const DeepCollectionEquality.unordered().equals(this, other);
 }
---
+++ test/app_bloc_test.dart
@@ -0,0 +1,9 @@
+import 'package:notes_bloc_app/models.dart';
+
+const Iterable<Note> mockNotes = [
+  Note(title: 'Note 1'),
+  Note(title: 'Note 2'),
+  Note(title: 'Note 3'),
+];
+
+class DummyNotesApi
